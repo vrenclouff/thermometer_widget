8f88ab73798fef8463eb34165b4dcd21e58530e8
Lukas Cerny <luc.cerny@gmail.com>
2014-07-16 19:26:30 +0200

    SSH connect transfer temperature file to local

---
+++ src/temperature/Main.java
@@ -0,0 +1,17 @@
+package temperature;
+
+import org.omg.CORBA.portable.InputStream;
+import org.omg.CORBA.portable.OutputStream;
+
+import com.jcraft.jsch.Channel;
+import com.jcraft.jsch.JSch;
+import com.jcraft.jsch.Session;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("test");
+	}
+}
+		
+		
